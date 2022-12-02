855ee04db9292092fc9b2862de04781a4485387c
Sheikh <sheikh@cybar.dev>
2022-12-02 02:30:18 -0400

    update resume builder script

--- resume.py
+++ resume.py
@@ -16,22 +16,15 @@ OUTPUT_DIR: str = "./assets/"
 
 class Resume:
     def __init__(self, parts: dict):
-        self.data: dict[str, str] = {}
-
         # add sections in order
+        self.data: dict[str, str] = {}
         for section, filename in parts.items():
-            self.data[section] = self.__readfile(SOURCE_DIR + filename)
+            with open(SOURCE_DIR + filename) as file:
+                self.data[section] = file.read()
 
     def build(self, filename: str = f"Resume_{AUTHOR.replace(' ', '_')}.md"):
         with open(OUTPUT_DIR + filename, "w") as file:
             file.write(self.__str__())
-
-    def __readfile(self, filename: str) -> str:
-        result: str = ""
-        with open(filename) as file:
-            result = file.read()
-
-        return result
 
     def __str__(self) -> str:
         return "\n".join(self.data.values()).strip()
--- src/header.md
+++ src/header.md
@@ -3,11 +3,11 @@
 <header>
   <p>Halifax, Nova Scotia B3J 2K9</p>
   <section style="display: flex; justify-content: space-around; margin-top: 1.1em;">
-    <a href="tel:+19028180048">(902) 818-0048</a>
-    <a href="https://github.com/cybardev">github.com/cybardev</a>
-    <a href="https://www.cybar.dev">www.cybar.dev</a>
-    <a href="https://www.linkedin.com/in/cybardev">linkedin.com/in/cybardev</a>
+    <a href="tel:+19028180048">+1 (902) 818-0048</a>
     <a href="mailto:sheikh@cybar.dev">sheikh@cybar.dev</a>
+    <a href="https://www.cybar.dev">www.cybar.dev</a>
+    <a href="https://github.com/cybardev">github.com/cybardev</a>
+    <a href="https://www.linkedin.com/in/cybardev">linkedin.com/in/cybardev</a>
   </section>
 </header>
 
