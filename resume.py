d21e796ecb75138e9b3badf01ccad1f0e331b00d
Sheikh <sheikh@cybar.dev>
2022-12-11 21:36:06 -0400

    Update resume.py

--- resume.py
+++ resume.py
@@ -118,6 +118,25 @@ def default_author():
     )
 
 
+class Address:
+    street: str
+    postal_code: str
+    city: str = "Halifax"
+    province: str = "NS"
+    province_full: str = "Nova Scotia"
+    country: str = "Canada"
+    
+    @property
+    def full_address(self):
+        return f"{self.street}, {self}"
+
+    def __str__(self) -> str:
+        return f"{self.city}, {self.province} {self.postal_code}"
+
+    def __repr__(self) -> str:
+        return f"{self.street},  \n" + str(self)
+
+
 @dataclass(kw_only=True)
 class Education:
     school: str
