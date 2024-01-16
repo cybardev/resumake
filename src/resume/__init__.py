633367fdbc282b3c0df209b3e285d3579c49762f
Sheikh <sheikh@cybar.dev>
2024-01-16 00:38:14 -0400

    refactor: use full import paths

--- src/resume/__init__.py
+++ src/resume/__init__.py
@@ -4,11 +4,11 @@
 __version__ = "1.0.0"
 
 # import everything needed for a resume
-from resume.components.address import Address
-from resume.components.author import Author
-from resume.components.education import Education
-from resume.components.experience import Experience
-from resume.components.project import Project
-from resume.components.resume import Resume
-from resume.components.skill import Skill
+from src.resume.components.address import Address
+from src.resume.components.author import Author
+from src.resume.components.education import Education
+from src.resume.components.experience import Experience
+from src.resume.components.project import Project
+from src.resume.components.resume import Resume
+from src.resume.components.skill import Skill
 from src.resume.utils import generate_resume
