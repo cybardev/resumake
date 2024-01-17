dabbb9fdec639cebec7725a6c75e7f64a0aaa47f
cybardev <sheikh@cybar.dev>
2024-01-17 00:46:21 -0400

    re: use proper pandoc arguments

--- src/resume/utils.py
+++ src/resume/utils.py
@@ -17,12 +17,45 @@ def xp_fmt(name, address, spec, date):
     )
 
 
-def md_to_pdf(md_file: str, pdf_file: str):
-    subprocess.run(["pandoc", md_file, "-o", pdf_file])
+def md_to_pdf(
+    md_file: str,
+    pdf_file: str,
+    html_template: str = "./src/template/resume.html",
+    css_template: str = "./src/template/resume.css",
+):
+    subprocess.run(
+        [
+            "pandoc",
+            md_file,
+            "-t",
+            "html",
+            f"--template={html_template}",
+            f"--css={css_template}",
+            "--metadata=title:Resume",
+            "--variable",
+            "papersize=letter",
+            "--variable",
+            "margin-top=0",
+            "--variable",
+            "margin-right=0",
+            "--variable",
+            "margin-bottom=0",
+            "--variable",
+            "margin-left=0",
+            "--pdf-engine-opt=--enable-local-file-access",
+            "-o",
+            pdf_file,
+        ]
+    )
 
 
 def generate_resume(author: Author, output_dir: str):
     # generate resume and output to file and standard output
     resume: Resume = Resume(author)
-    resume.build(f"Resume_{resume.author.name.replace(' ', '_')}.md", output_dir)
+    filename = f"Resume_{resume.author.name.replace(' ', '_')}"
+    resume.build(f"{filename}.md", output_dir)
+    md_to_pdf(
+        f"{output_dir.rstrip('/')}/{filename}.md",
+        f"{output_dir.rstrip('/')}/{filename}.pdf",
+    )
     print(resume)
