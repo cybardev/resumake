47e67cc1cfbc57d98ea4d81e761a49cb40737f1a
cybardev <sheikh@cybar.dev>
2024-02-12 00:41:43 -0400

    feat: new project specification

--- src/resumake/components/project.py
+++ src/resumake/components/project.py
@@ -1,10 +1,15 @@
+from dataclasses import dataclass
+
 from .experience import Experience
 
 
+@dataclass
 class Project(Experience):
-    """Alias for Experience class
+    name: str
+    url: str
+    desc: str
+    skills: list[str]
 
-    Main difference is that addresses are links instead of locations
-    """
-
-    pass
+    def __str__(self) -> str:
+        skills = ", ".join((f"`{skill}`" for skill in self.skills))
+        return f"**{self.name}** _({self.url})_ {self.desc} [{skills}]"
