cf8ee287aab2a3ae6bc1bdcc405b7f4620d669d5
cybardev <sheikh@cybar.dev>
2024-02-14 11:36:00 -0400

    feat: change small project description to ProjectSummary and reinstate Project as large description

--- src/resumake/components/project.py
+++ src/resumake/components/project.py
@@ -1,12 +1,35 @@
 from dataclasses import dataclass
 
+from ..utils import url_fmt
+from .experience import Experience
+
+
+class Project(Experience):
+    def __init__(
+        self,
+        name: str,
+        url: str,
+        role: str,
+        start: str,
+        end: str,
+        attributes: list[str],
+    ):
+        super().__init__(
+            name=name,
+            address=url_fmt(url),
+            role=role,
+            start=start,
+            end=end,
+            attributes=attributes,
+        )
+
 
 @dataclass(kw_only=True)
-class Project:
+class ProjectSummary:
     name: str
     url: str
     desc: str
     skills: list[str]
 
     def __str__(self) -> str:
-        return f"**{self.name}** ([{self.url}](https://{self.url})) {self.desc} **[{', '.join(self.skills)}]**"
+        return f"**{self.name}** ({url_fmt(self.url)}) {self.desc} **[{', '.join(self.skills)}]**"
