ae49d91f8cba2951a9cd9e3367a9cfcc84e55e11
cybardev <sheikh@cybar.dev>
2024-07-10 12:10:10 -0300

    re: restructuring

--- resumake.sh
+++ resumake.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
 echo "---" >resume.md
-cat resume/cybardev.yml >>resume.md
+cat resume.yml >>resume.md
 echo "---" >>resume.md
 
-pandoc -s resume.md \
-  -t html --template=template/resume.html \
+AUTHOR=$(head -n 1 resume.yml | cut -f 2- -d " " | tr " " "_")
+
+pandoc -s resume.md -t html \
+  --template=resources/template.html --css=resources/template.css \
   --metadata=title:Resume --variable papersize=letter \
   --variable margin-top=0 --variable margin-right=0 \
   --variable margin-bottom=0 --variable margin-left=0 \
   --pdf-engine-opt=--enable-local-file-access \
-  -o resume.pdf
+  -o static/assets/Resume_${AUTHOR}.pdf
 
 rm -f resume.md
