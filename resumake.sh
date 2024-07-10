b20956b75d098c07b1e1ef55537a1afe149e3ec1
cybardev <sheikh@cybar.dev>
2024-07-10 12:20:48 -0300

    feat: add margin as CLI args

--- resumake.sh
+++ resumake.sh
@@ -5,11 +5,12 @@ cat resume.yml >>resume.md
 echo "---" >>resume.md
 
 AUTHOR=$(head -n 1 resume.yml | cut -f 2- -d " " | tr " " "_")
+[ -z $1 ] && MARGIN=2 || MARGIN=$1
 
 pandoc -s resume.md -t html \
   --template=resources/template.html --css=resources/template.css \
   --metadata=title:Resume --variable papersize=letter \
-  --variable margin-top=0 --variable margin-right=0 \
+  --variable margin-top=$MARGIN --variable margin-right=0 \
   --variable margin-bottom=0 --variable margin-left=0 \
   --pdf-engine-opt=--enable-local-file-access \
   -o static/assets/Resume_${AUTHOR}.pdf
