d1d9f61508ef5620aa3465a20cc3cc828245dd64
cybardev <sheikh@cybar.dev>
2024-07-11 15:19:44 -0300

    fix: proper escaping of special character

--- resumake.sh
+++ resumake.sh
@@ -2,7 +2,7 @@
 
 # variables
 [ -z $1 ] && MARGIN=2 || MARGIN=$1
-AUTHOR='$(head -n 1 resume.yml | cut -f 2- -d " " | tr " " "_")'
+AUTHOR="$(head -n 1 resume.yml | cut -f 2- -d ' ' | tr ' ' '_')"
 TEMPFILE="resources/_resume.md"
 OUTFILE="static/assets/Resume_${AUTHOR}.pdf"
 
@@ -12,13 +12,13 @@ cat resume.yml >>${TEMPFILE}
 echo "---" >>${TEMPFILE}
 
 # generate resume PDF
-pandoc -s ${TEMPFILE} -t html \
-  --template=resources/template.html --css=resources/template.css \
+pandoc -s "${TEMPFILE}" -t html \
+  --template=resources/template.html \
   --metadata=title:Resume --variable papersize=letter \
   --variable margin-top=$MARGIN --variable margin-right=0 \
   --variable margin-bottom=0 --variable margin-left=0 \
   --pdf-engine-opt=--enable-local-file-access \
-  -o ${OUTFILE}
+  -o "${OUTFILE}"
 
 # clean up intermediate files
 rm -f ${TEMPFILE}
