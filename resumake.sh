71ca69f60a8c3576791aca9cb604642294828596
Sheikh <sheikh@cybar.dev>
2024-07-11 10:18:17 -0300

    re: extract repeated values into variables

--- resumake.sh
+++ resumake.sh
@@ -1,18 +1,24 @@
 #!/usr/bin/env bash
 
-echo "---" >resume.md
-cat resume.yml >>resume.md
-echo "---" >>resume.md
-
-AUTHOR=$(head -n 1 resume.yml | cut -f 2- -d " " | tr " " "_")
+# variables
 [ -z $1 ] && MARGIN=2 || MARGIN=$1
+AUTHOR='$(head -n 1 resume.yml | cut -f 2- -d " " | tr " " "_")'
+TEMPFILE="resources/_resume.md"
+OUTFILE="static/assets/Resume_${AUTHOR}.pdf"
 
-pandoc -s resume.md -t html \
+# create metadata file with resume info
+echo "---" >${TEMPFILE}
+cat resume.yml >>${TEMPFILE}
+echo "---" >>${TEMPFILE}
+
+# generate resume PDF
+pandoc -s ${TEMPFILE} -t html \
   --template=resources/template.html --css=resources/template.css \
   --metadata=title:Resume --variable papersize=letter \
   --variable margin-top=$MARGIN --variable margin-right=0 \
   --variable margin-bottom=0 --variable margin-left=0 \
   --pdf-engine-opt=--enable-local-file-access \
-  -o static/assets/Resume_${AUTHOR}.pdf
+  -o ${OUTFILE}
 
-rm -f resume.md
+# clean up intermediate files
+rm -f ${TEMPFILE}
