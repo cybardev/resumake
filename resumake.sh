79765f5bca73590a3aaf8af31bd83bf3c87e58ba
cybardev <sheikh@cybar.dev>
2024-08-18 09:46:44 -0300

    feat: specify YAML data file as CLI arg

--- resumake.sh
+++ resumake.sh
@@ -1,21 +1,22 @@
 #!/usr/bin/env bash
 
 # variables
-[ -z $1 ] && MARGIN=2 || MARGIN=$1
-AUTHOR="$(head -n 1 resume.yml | cut -f 2- -d ' ' | tr ' ' '_')"
-TEMPFILE="resources/_resume.md"
-OUTFILE="static/assets/Resume_${AUTHOR}.pdf"
+[ -z $1 ] && INFILE="resume.yml" || INFILE=$1
+[ -z $2 ] && MARGIN=2 || MARGIN=$2
+AUTHOR="$(head -n 1 ${INFILE} | cut -f 2- -d ' ' | tr ' ' '_')"
+TEMPFILE="_resume.md"
+OUTFILE="Resume_${AUTHOR}.pdf"
 
 # create metadata file with resume info
 echo "---" >${TEMPFILE}
-cat resume.yml >>${TEMPFILE}
+cat ${INFILE} >>${TEMPFILE}
 echo "---" >>${TEMPFILE}
 
 # generate resume PDF
 pandoc -s "${TEMPFILE}" -t html \
   --template=resources/template.html \
   --metadata=title:Resume --variable papersize=letter \
-  --variable margin-top=$MARGIN --variable margin-right=0 \
+  --variable margin-top=${MARGIN} --variable margin-right=0 \
   --variable margin-bottom=0 --variable margin-left=0 \
   --pdf-engine-opt=--enable-local-file-access \
   -o "${OUTFILE}"
